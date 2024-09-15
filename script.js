0df8dc9012dd6cfae43e4bd65433131b6b49b7a0
pqba <femimakinde@icloud.com>
2024-09-15 18:39:55 -0500

    Add dark and light mode js and icons

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ window.addEventListener('load', function () {
     
 })
 
+// Add theme change detector
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('theme-toggle');
+    let currentTheme = localStorage.getItem('theme') || 'light';
+  
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    toggleButton.addEventListener('click', () => {
+      let newTheme = currentTheme === 'light' ? 'dark' : 'light';
+      document.documentElement.setAttribute('data-theme', newTheme);
+      localStorage.setItem('theme', newTheme);
+      currentTheme = newTheme;
+      setThemeImg(currentTheme);
+    });
+    setThemeImg(currentTheme);
+});
+
+// Changes image path based on theme value (light or dark)
+function setThemeImg(theme) {
+    const toggleImage = document.getElementById('theme-image');
+    if(theme == 'light') {
+        toggleImage.src = 'static/moon_graphic.png';
+    }
+    else if (theme == 'dark') {
+        toggleImage.src = 'static/sun_graphic.png';
+    }
+    else {
+        console.error("Invalid theme property.");
+    }
+}
